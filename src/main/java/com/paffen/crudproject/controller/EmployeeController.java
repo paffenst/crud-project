8cd56f0723f78ccf5ab7c5b8f4c5283c69263cb0
paffenst <paffen.st@gmail.com>
2024-01-30 11:29:12 +0100

    add operation(post request) on service and controller

--- src/main/java/com/paffen/crudproject/controller/EmployeeController.java
+++ src/main/java/com/paffen/crudproject/controller/EmployeeController.java
@@ -1,7 +1,19 @@
 package com.paffen.crudproject.controller;
 
+import com.paffen.crudproject.entity.Employee;
+import com.paffen.crudproject.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+    @PostMapping("/save/employee")
+    public Employee saveEmployee(@RequestBody Employee employee){
+      return employeeService.saveEmployee(employee);
+    }
 }
--- src/main/java/com/paffen/crudproject/service/EmployeeService.java
+++ src/main/java/com/paffen/crudproject/service/EmployeeService.java
@@ -1,7 +1,16 @@
 package com.paffen.crudproject.service;
 
+import com.paffen.crudproject.DAO.EmployeeDao;
+import com.paffen.crudproject.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmployeeService {
+
+    @Autowired
+    private EmployeeDao employeeDao;
+    public Employee saveEmployee(Employee employee){
+       return employeeDao.save(employee);
+    }
 }
